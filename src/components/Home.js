2a09dbf7cfadc4ecc87f6d65ac56e83478748850
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-01-13 02:34:04 +0300

    Completed Assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const divStyle = {
+  color:'firebrick'
+}
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const mystyle ={color:'firebrick'};
+  return (<div id="home" style={divStyle}>
+   <h1 style = {mystyle}>{name} is a Web Developer from {city}</h1>
+
+  </div>
+  );
 }
+
 
 export default Home;
